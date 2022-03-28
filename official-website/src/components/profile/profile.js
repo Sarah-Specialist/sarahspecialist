20689fb700103ae45914d7abda7db45604e6d1a2
Sarah Specialist <sarah.specialist@gmail.com>
2022-03-28 11:33:05 +0800

    changed photo

--- official-website/src/components/profile/profile.js
+++ official-website/src/components/profile/profile.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import photo from './photo.png';
+import photograph from './photograph.jpg';
 
 export default function Profile() {
     return (
         <>
-        <img src={photo} 
+        <img src={photograph} 
             alt="profile"
-            height="200" width="200"
-            style={{backgroundColor: "inherit"}} />
+            height="270" width="260"
+            style={{backgroundColor: "inherit", marginTop: -50, marginBotton: -20, paddingLeft: 20}} />
         </>
     )
 }
